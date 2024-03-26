b213a15b8a89fa2c9c4da075695ad81c4a8ff4ea
Oleg Klimenko <klimmail@rambler.ru>
2024-03-26 13:40:43 +0300

    initial

---
+++ drf-auth-and-validation/api_with_restrictions/advertisements/serializers.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+        count_status = 0
+        # TODO: добавьте требуемую валидацию
+        for value in data:
+            if value.get('status', '') == 'True':
+                count_status += 1
+                if count_status > 10:
+                    print('превышено число открытых постов')
+                    break
+            count_status -= 1
+        return data
